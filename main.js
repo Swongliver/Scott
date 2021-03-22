730756e2bded20b8b0fe7942aaa77ca4801fa892
Swongliver <81005472+Swongliver@users.noreply.github.com>
2021-03-22 17:40:22 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function imagesmall()
+{
+  document.getElementById("img").style.width="300px";
+      document.getElementById("img1").style.width="300px";
+          document.getElementById("img2").style.width="300px";
+          document.getElementById("img3").style.width="300px";
+
+
+
+
+}
+ 
+function setDate(){
+    document.getElementById("date").innerHTML=new Date().toDateString();
+}
